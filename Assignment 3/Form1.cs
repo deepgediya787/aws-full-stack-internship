e3eb8491f361e6bec2c78ba320466be9f72f3fa8
deepgediya787 <109960727+deepgediya787@users.noreply.github.com>
2023-07-19 16:33:56 +0530

    Add files via upload

---
+++ Assignment 3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                SqlConnection con = new SqlConnection("Data Source=DESKTOP-I8VMEJ3;Initial Catalog=DESKTOP-I8VMEJ3;Integrated Security=True;Pooling=False");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into dbo values(@Name,@Email,@Contact,@feed) ", con);
+               cmd.Parameters.AddWithValue("@Name",textBox1.Text);
+               cmd.Parameters.AddWithValue("@Email",textBox2.Text);
+               cmd.Parameters.AddWithValue("@Contact",Convert.ToInt64(textBox4.Text));
+               cmd.Parameters.AddWithValue("@feed",textBox3.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                button1.BackColor = SystemColors.Window;
+            
+                
+        }
+    }
+}
---
+++ Assignment 3/feeddb.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Name] VARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Email] VARCHAR(70) NOT NULL, 
+    [Contact] BIGINT NULL, 
+    [feed] VARCHAR(MAX) NULL
+)
